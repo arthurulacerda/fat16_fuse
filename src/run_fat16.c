99c2598a6adbe96bdf62712298c6ee1127f8a57f
Murillo de Paula <murillopaula@inf.ufg.br>
2017-05-13 22:26:31 -0300

    Decodifica o Root e tenta acessar os clusters

--- src/run_fat16.c
+++ src/run_fat16.c
@@ -5,44 +5,48 @@
 #include "sector.h"
 #include "sector.c"
 
+typedef uint8_t BYTE;
+typedef uint16_t WORD;
+typedef uint32_t DWORD;
+
 typedef struct {
-  uint8_t BS_jmpBoot[3];
-  uint8_t BS_OEMName[8];
-  uint16_t BPB_BytsPerSec;
-  uint8_t BPB_SecPerClus;
-  uint16_t BPB_RsvdSecCnt;
-  uint8_t BPB_NumFATS;
-  uint16_t BPB_RootEntCnt;
-  uint16_t BPB_TotSec16;
-  uint8_t BPB_Media;
-  uint16_t BPB_FatSz16;
-  uint16_t BPB_SecPerTrk;
-  uint16_t BPB_NumHeads;
-  uint32_t BPB_HiddSec;
-  uint32_t BPB_TotSec32;
-  uint8_t BS_DrvNum;
-  uint8_t BS_Reserved1;
-  uint8_t BS_BootSig;
-  uint32_t BS_VollID;
-  uint8_t BS_VollLab[11];
-  uint8_t BS_FilSysType[8];
-  uint8_t Hex1[448];
-  uint16_t Signature_word;
+  BYTE BS_jmpBoot[3];
+  BYTE BS_OEMName[8];
+  WORD BPB_BytsPerSec;
+  BYTE BPB_SecPerClus;
+  WORD BPB_RsvdSecCnt;
+  BYTE BPB_NumFATS;
+  WORD BPB_RootEntCnt;
+  WORD BPB_TotSec16;
+  BYTE BPB_Media;
+  WORD BPB_FatSz16;
+  WORD BPB_SecPerTrk;
+  WORD BPB_NumHeads;
+  DWORD BPB_HiddSec;
+  DWORD BPB_TotSec32;
+  BYTE BS_DrvNum;
+  BYTE BS_Reserved1;
+  BYTE BS_BootSig;
+  DWORD BS_VollID;
+  BYTE BS_VollLab[11];
+  BYTE BS_FilSysType[8];
+  BYTE Hex1[448];
+  WORD Signature_word;
 } __attribute__ ((packed)) BPB_BS;
 
 typedef struct {
-  uint8_t DIR_Name[11];
-  uint8_t DIR_Attr;
-  uint8_t DIR_NTRes;
-  uint8_t DIR_CrtTimeTenth;
-  uint16_t DIR_CrtTime;
-  uint16_t DIR_CrtDate;
-  uint16_t DIR_LstAccDate;
-  uint16_t DIR_FstClusHI;
-  uint16_t DIR_WrtTime;
-  uint16_t DIR_WrtDate;
-  uint16_t DIR_FstClusLO;
-  uint32_t DIR_FileSize;
+  BYTE DIR_Name[11];
+  BYTE DIR_Attr;
+  BYTE DIR_NTRes;
+  BYTE DIR_CrtTimeTenth;
+  WORD DIR_CrtTime;
+  WORD DIR_CrtDate;
+  WORD DIR_LstAccDate;
+  WORD DIR_FstClusHI;
+  WORD DIR_WrtTime;
+  WORD DIR_WrtDate;
+  WORD DIR_FstClusLO;
+  DWORD DIR_FileSize;
 } __attribute__ ((packed)) DIR_ENTRY;
 
 /** Reads the first 512 bytes of the image, that represents the BPB;
@@ -50,51 +54,48 @@ typedef struct {
 * @param fd: File descriptor to read the Bpb sector.
 */
 
-void printBPB(BPB_BS *Bpb) {
+void printBPB(BPB_BS *Bpb, FILE *out) {
   int i;
   for (i = 0; i < 11; i++) {
-    printf("%c", Bpb->BS_VollLab[i]);
+    fprintf(out, "%c", Bpb->BS_VollLab[i]);
   }
-  printf("\n");
+  fprintf(out, "\n");
   for (i = 0; i < 8; i++) {
-    printf("%c", Bpb->BS_FilSysType[i]);
+    fprintf(out, "%c", Bpb->BS_FilSysType[i]);
   }
-  printf("\n");
-  printf("OEMNAME: %s\n", Bpb->BS_OEMName);
-  printf("BytsPersec: %d\n", Bpb->BPB_BytsPerSec);
-  printf("SecPerClus: %d\n", Bpb->BPB_SecPerClus);
-  printf("NumFATS: %x\n", Bpb->BPB_NumFATS);
-  printf("VollID: %d\n", Bpb->BS_VollID);
-  printf("RootEntCont: %d\n\n", Bpb->BPB_RootEntCnt);
+  fprintf(out, "\n");
+  fprintf(out, "OEMNAME: %s\n", Bpb->BS_OEMName);
+  fprintf(out, "BytsPersec: %d\n", Bpb->BPB_BytsPerSec);
+  fprintf(out, "SecPerClus: %d\n", Bpb->BPB_SecPerClus);
+  fprintf(out, "NumFATS: %x\n", Bpb->BPB_NumFATS);
+  fprintf(out, "VollID: %d\n", Bpb->BS_VollID);
+  fprintf(out, "RootEntCont: %d\n\n", Bpb->BPB_RootEntCnt);
 }
 
-void printDIR(DIR_ENTRY *Dir) {
+void printDIR(DIR_ENTRY *Dir, FILE *out) {
   int i;
-  printf("Name: ");
+  fprintf(out, "Name: ");
   for (i = 0; i < 11; i++) {
-    printf("%c", Dir->DIR_Name[i]);
+    fprintf(out, "%c", Dir->DIR_Name[i]);
   }
-  printf("\n");
-  printf("Attr: %x\n", Dir->DIR_Attr);
-  printf("NTRes: %d\n", Dir->DIR_NTRes);
-  printf("CrtTimeTenth: %d\n", Dir->DIR_CrtTimeTenth);
-  printf("CrtTime: %d\n", Dir->DIR_CrtTime);
-  printf("CrtDate: %d\n", Dir->DIR_CrtDate);
-  printf("LstAccDate: %d\n", Dir->DIR_LstAccDate);
-  printf("FstClusHI: %d\n", Dir->DIR_FstClusHI);
-  printf("WrtTime: %d\n", Dir->DIR_WrtTime);
-  printf("WrtDate: %d\n", Dir->DIR_WrtDate);
-  printf("FstClusLO: %d\n", Dir->DIR_FstClusLO);
-  printf("FileSize: %d\n", Dir->DIR_FileSize);
+  fprintf(out, "\n");
+  fprintf(out, "Attr: %x\n", Dir->DIR_Attr);
+  fprintf(out, "NTRes: %d\n", Dir->DIR_NTRes);
+  fprintf(out, "CrtTimeTenth: %d\n", Dir->DIR_CrtTimeTenth);
+  fprintf(out, "CrtTime: %d\n", Dir->DIR_CrtTime);
+  fprintf(out, "CrtDate: %d\n", Dir->DIR_CrtDate);
+  fprintf(out, "LstAccDate: %d\n", Dir->DIR_LstAccDate);
+  fprintf(out, "FstClusHI: %d\n", Dir->DIR_FstClusHI);
+  fprintf(out, "WrtTime: %d\n", Dir->DIR_WrtTime);
+  fprintf(out, "WrtDate: %d\n", Dir->DIR_WrtDate);
+  fprintf(out, "FstClusLO: %d\n", Dir->DIR_FstClusLO);
+  fprintf(out, "FileSize: %d\n\n", Dir->DIR_FileSize);
 }
 
-/*void readFileFAT(FILE* fd, DIR *Dir, uint16_t fatPosition){
+/*void readFileFAT(FILE* fd, DIR *Dir, WORD fatPosition){
   int fatSector = fatPosition / 32; //512 has 32 slots of 16bytes.
 
 }*/
-
-void searchRootDIR(DIR_ENTRY *Dir, uint8_t *buffer, uint16_t RootDirSectors) {
-}
 
 int main(int argc, char **argv) {
   if (argv[1] == NULL) {
@@ -102,47 +103,56 @@ int main(int argc, char **argv) {
     exit(0);
   }
   FILE *fd = fopen(argv[1], "rb");
-  uint8_t buffer[BYTES_PER_SECTOR];
+  FILE *out = fopen("out", "w");
+  BYTE buffer[BYTES_PER_SECTOR];
+
+  /* BPB */
   BPB_BS Bpb;
 
   /* Reading BPB */
   sector_read(fd, 0, &Bpb);
-  printBPB(&Bpb);
+  printBPB(&Bpb, out);
 
-  uint16_t RootEntCnt = Bpb.BPB_RootEntCnt;
-  DIR_ENTRY Root[RootEntCnt];
+  /* Root directory */
+  DIR_ENTRY *Root = calloc(Bpb.BPB_RootEntCnt, sizeof *Root);
+  if (!Root) exit(1);
 
-  /* Reading Root Directory
-   * Jumps both FAT and the BPB sector. */
+  /* First sector of the root directory */
   unsigned int FirstRootDirSecNum = Bpb.BPB_RsvdSecCnt + (Bpb.BPB_FatSz16 * Bpb.BPB_NumFATS);
+
+  /* Reading root directory */
+  int RootDirCnt = 1, i;
   sector_read(fd, FirstRootDirSecNum, &buffer);
-  uint16_t RootDirSectors = ((Bpb.BPB_RootEntCnt * 32) + (Bpb.BPB_BytsPerSec - 1)) / Bpb.BPB_BytsPerSec;
-  printf("RootDirSectors: %d\n", RootDirSectors);
+  memcpy(&Root[0], buffer, 32);
+  for (i = 1; i < Bpb.BPB_RootEntCnt && Root[i - 1].DIR_Name[0] != 0x00; i++) {
+    printDIR(&Root[i - 1], out);
+    memcpy(&Root[i], &buffer[i * 32], 32);
 
-  /* Searching for file/directory in the Root Directory */
-  int i, j, k;
-  memcpy(&Root[0], &buffer, 32);
-  if (Root[0].DIR_Name[0] != 0) {
-    printDIR(&Root[0]);
-    printf("\n");
-  }
-  for (k = 16; k <= RootDirSectors; k += 16) {
-    for (j = 1; j <= 16; j++) {
-      for (i = 0; i < 32; i++) {
-        buffer[i] = buffer[i + (j * 32)];
-      }
-      memcpy(&Root[j], &buffer, 32);
-      if (Root[j].DIR_Name[0] != 0) {
-        printDIR(&Root[j]);
-        printf("\n");
-      }
+    /* End of bytes for this sector (1 sector == 512 bytes == 16 DIR entries)
+     * Read next sector */
+    if (i % 16 == 0) {
+      sector_read(fd, FirstRootDirSecNum + RootDirCnt, &buffer);
+      RootDirCnt++;
     }
-    /* Out of bytes in this sector (1 sector == 512 bytes == 16 dir entries)
-     * Read one more sector */
-    sector_read(fd, FirstRootDirSecNum, buffer);
   }
-
   //readFileFAT(fd,&root,root.DIR_FstClusHI);
+
+  /* First sector of the Data Region */
+  WORD RootDirSectors = ((Bpb.BPB_RootEntCnt * 32) + (Bpb.BPB_BytsPerSec - 1)) / Bpb.BPB_BytsPerSec;
+
+  /* Number of FAT entries */
+
+  /* FAT */
+  WORD *Fat = calloc(Bpb.BPB_FatSz16, sizeof *Fat);
+  if (!Fat) exit(2);
+
+  unsigned int ClusterN = Root[0].DIR_FstClusLO;
+  unsigned int FATOffset = ClusterN * 2;
+  unsigned int FatSecNum = Bpb.BPB_RsvdSecCnt + (FATOffset / BYTES_PER_SECTOR);
+  unsigned int FatEntOffset = FATOffset % BYTES_PER_SECTOR;
+  sector_read(fd, FatSecNum, &buffer);
+  WORD FatClusEntryVal = *((WORD *) &buffer[FatEntOffset]);
+  printf("%d\n", FatClusEntryVal);
 
   return 0;
 }
